19c371a4b29e401c2d017ef70e75d1236be0a636
Fabo1808 <fabianjga@gmail.com>
2021-10-19 14:36:53 -0400

    1

--- src/app.js
+++ src/app.js
@@ -2,10 +2,24 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
+import "./assets/img/online-domain-logo.png";
 import "./assets/img/4geeks.ico";
 
+// lists
+var pronoun = ["I", "She", "The", "We", "It"];
+var adj = ["pink", "huge", "sweet", "smart"];
+var noun = ["paper", "history", "time", "money"];
+var result = "";
+
+// function
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let j = 0; j < adj.length; j++) {
+      for (let k = 0; k < noun.length; k++) {
+        result += pronoun[i] + adj[j] + noun[k] + ".com" + "<br>";
+        document.querySelector("div > p.alert").innerHTML = result;
+        console.log(pronoun[i] + adj[j] + noun[k] + ".com");
+      }
+    }
+  }
 };
